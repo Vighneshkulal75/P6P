3525a6926d11ffee3366e3c4a764d40226f6ac32
Vigneshaa <vighneshhombadi@gmail.com>
2025-06-07 22:03:37 +0530

    first

---
+++ src/main/java/org/example/P21.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class P21 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("i am vighnesh");
+    }
+}
